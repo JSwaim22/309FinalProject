052d80a3da677aff5c3e3b573028f155128e5d53
JSwaim22 <52038890+JSwaim22@users.noreply.github.com>
2021-11-18 23:35:40 -0500

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -6,84 +6,30 @@ using namespace std;
 #define empty       0
 #define red         1
 #define yellow      2
+#define p1          false
+#define p2          true
 
-//  Prime Locations
-#define a1          2
-#define a2          19
-#define a3          47
-#define a4          79
-#define a5          109
-#define a6          151
+class four_in_a_row {
+    public:
+        int color;
+        int key;
+        int filled;
+        four_in_a_row(int k, int c, int f){
+            key = k;
+            color = c;
+            filled = f;
+        }
+};
 
-#define b1          3
-#define b2          23
-#define b3          53
-#define b4          83
-#define b5          113
-#define b6          157
-
-#define c1          5
-#define c2          29
-#define c3          59
-#define c4          89
-#define c5          127
-#define c6          163
-
-#define d1          7
-#define d2          31
-#define d3          61
-#define d4          97
-#define d5          131
-#define d6          167
-
-#define e1          11
-#define e2          37
-#define e3          67
-#define e4          101
-#define e5          137
-#define e6          173
-
-#define f1          13
-#define f2          41
-#define f3          71
-#define f4          103
-#define f5          139
-#define f6          179
-
-#define g1          17
-#define g2          43
-#define g3          73
-#define g4          107
-#define g5          149
-#define g6          181
-
-
-//  Prime Products
-#define hor1_1      0
-#define hor1_2      1
-#define hor1_3      2
-#define hor1_4      3
-#define hor2_1      4
-#define hor2_2      5
-#define hor2_3      6
-#define hor2_4      7
-#define hor3_1      8
-#define hor3_2      9
-#define hor3_3      10
-#define hor3_4      11
-#define hor4_1      12
-#define hor4_2      13
-#define hor4_3      14
-#define hor4_4      15
-#define hor5_1      16
-#define hor5_2      17
-#define hor5_3      18
-#define hor5_4      19
-#define hor6_1      20
-#define hor6_2      21
-#define hor6_3      22
-#define hor6_4      23
-#define ver1_1
+class discs {
+    public:
+        int location;
+        int color;
+        discs(int l, int c){
+            location = l;
+            color = c;
+        }
+};
 
 int keys [69] = {   // Horizontal Group Keys
                     210, 1155, 5005, 17017, 392863, 765049, 1363783, 2022161, 8965109, 12780049, 
@@ -105,82 +51,55 @@ int locs [42] = {   2,      3,      5,      7,      11,     13,     17,
                     109,    113,    127,    131,    137,    139,    149,
                     151,    157,    163,    167,    173,    179,    181     };
 
-class four_in_a_row {
-    private:
+list<four_in_a_row> groups;
 
-    public:
-        int color;
-        int key;
-        int filled;
-        four_in_a_row(int k, int c, int f){
-            key = k;
-            color = c;
-            filled = f;
-        }
-
-        ~four_in_a_row() {
-            cout << "not an option" << endl;
-        }
-};
-
-class discs {
-    private:
-
-    public:
-        int location;
-        int color;
-        discs(int l, int c){
-            location = l;
-            color = c;
-        }
-};
-
-/*
 bool check_win(int index, int color) {
     discs *new_disc = new discs(locs[index], color);
 
-    for(auto it = groups.begin(); it != l.end(); it++) {
-        if(groups.front().key % pieces.front().location == 0) {
-            if(groups.front().color == none) {
-                groups.front().color = pieces.front().color;
-                groups.front().filled++;
-            } else if(groups.front().color == pieces.front().color) {
-                groups.front().filled++;
+    bool win = false;
+
+    for(list<four_in_a_row>::iterator it = groups.begin(); it != groups.end(); it++) {
+        if(it->key % new_disc->location == 0) {
+            if(it->color == none || it->color == new_disc->color) {
+                it->color = new_disc->color;
+                it->filled++;
+                if(it->filled == 4) {
+                    win = true;
+                }
             } else {
-                groups.pop_front();
+                it = groups.erase(it);
+                it--;
             }
         }
     }
+
+    delete new_disc;
+    return win;
 }
 
-*/
-
 int main() {
-
-
-    list<four_in_a_row> groups;
-    list<discs> pieces;
 
     for(int i = 0; i < 69; i++) {
         groups.push_back(four_in_a_row(keys[i], none, empty));
     }
 
-    cout << groups.front().key << endl;
-
-    pieces.push_back(discs(a1, red));
-
-    if(groups.front().key % pieces.front().location == 0) {
-        if(groups.front().color == none) {
-            groups.front().color = pieces.front().color;
-            groups.front().filled++;
-        } else if(groups.front().color == pieces.front().color) {
-            groups.front().filled++;
+    bool stop = false;
+    int input;
+    bool player = p1;
+    
+    while(stop == false) {
+        cin >> input;
+        if(player == p1) {
+            if(check_win(input, red)) {
+                cout << "p1 wins!";
+            }
         } else {
-            groups.pop_front();
+            if(check_win(input, yellow)) {
+                cout << "p2 wins!";
+            }   
         }
+        player = !player;
     }
-
-    cout << "Hello World!" << groups.front().color << endl;
 
     return 0;
 }
